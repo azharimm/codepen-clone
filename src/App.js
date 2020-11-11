c5d52d36427d1997c1daf066ace096fed610ec19
Azhari Marzan <azhari.marzan@gmail.com>
2020-11-11 15:52:02 +0700

    ^ base layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import Editor from "./components/Editor";
 
 function App() {
+    const [html, setHtml] = useState("");
+    const [css, setCss] = useState("");
+    const [javascript, setJavascript] = useState("");
     return (
-        <div className="App">
-            <h1>Hello World</h1>
-        </div>
+        <>
+            <div className="pane top-pane">
+                <Editor
+                    language="xml"
+                    displayName="HTML"
+                    value={html}
+                    onChange={setHtml}
+                />
+                <Editor
+                    language="css"
+                    displayName="CSS"
+                    value={css}
+                    onChange={setCss}
+                />
+                <Editor
+                    language="javascript"
+                    displayName="Javascript"
+                    value={javascript}
+                    onChange={setJavascript}
+                />
+            </div>
+            <div className="pane">
+                <iframe
+                    title="output"
+                    sandbox="allow-scripts"
+                    frameBorder="0"
+                    width="100%"
+                    height="100%"
+                />
+            </div>
+        </>
     );
 }
 
